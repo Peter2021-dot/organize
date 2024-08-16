8b467e06e20472635c88b0a3e3edb8f460b849b5
John Peterson Valcourt <84840840+Peter2021-dot@users.noreply.github.com>
2024-08-16 22:28:30 +0800

    Add files via upload

---
+++ src/app/contact/page.tsx
@@ -0,0 +1,62 @@
+import React from "react";
+
+const ContactPage = () => {
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100 p-4 dark:text-white">
+      <div className="w-full max-w-2xl bg-white rounded-2xl shadow-md p-6 dark:bg-gray-800">
+        <h1 className="text-2xl font-bold mb-6">Contact Us</h1>
+        <form className="space-y-4">
+          <div>
+            <label
+              htmlFor="name"
+              className="block text-sm font-medium text-gray-700 dark:text-white"
+            >
+              Name
+            </label>
+            <input
+              type="text"
+              id="name"
+              className="mt-1 block w-full px-3 py-2 border dark:text-white rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+            />
+          </div>
+          <div>
+            <label
+              htmlFor="email"
+              className="block text-sm font-medium text-gray-700 dark:text-white"
+            >
+              Email
+            </label>
+            <input
+              type="email"
+              id="email"
+              className="mt-1 block w-full px-3 py-2 border  border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+            />
+          </div>
+          <div>
+            <label
+              htmlFor="message"
+              className="block text-sm font-medium text-gray-700 dark:text-white"
+            >
+              Message
+            </label>
+            <textarea
+              id="message"
+              rows="4"
+              className="mt-1 block w-full px-3 dark:text-white py-2 border rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+            ></textarea>
+          </div>
+          <div className="flex justify-end">
+            <button
+              type="submit"
+              className="inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium  rounded-2xl text-white bg-blue-500 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+            >
+              Send
+            </button>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default ContactPage;
